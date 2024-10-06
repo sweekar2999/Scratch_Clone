4c172e3e9e177ac6ee2b8d842bbdf2a87745084e
sweekar <sweekardhaigude103@gmail.com>
2024-10-06 17:14:47 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import Sidebar from "./components/Sidebar";
 import MidArea from "./components/MidArea";
 import PreviewArea from "./components/PreviewArea";
-import Navbar from "./components/NavBar"; 
+
 
 
 export default function App() {
   return (
     <div className="bg-blue-100 font-sans">
-    <Navbar/>
+       <nav className="bg-light-blue-500 p-4">  
+      <div className="container mx-auto flex justify-start">
+        <a href="/">
+          <img 
+            src="/Logo.png"  
+            alt="MIT Scratch Logo" 
+            className="h-10 w-auto" 
+          />
+        </a>
+      </div>
+    </nav>
       <div className="h-screen overflow-hidden flex flex-row">
         <div className="flex-1 h-screen overflow-hidden flex flex-row bg-white border-t border-r border-gray-200 rounded-tr-xl mr-2">
           <Sidebar /> <MidArea />
